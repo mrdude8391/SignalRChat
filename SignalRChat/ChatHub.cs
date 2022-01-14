d72cc784f3f5f0c9dab0459fcc1b95184ce6ba81
jasminem <jasminem@NXDEV4>
2022-01-14 11:28:34 -0500

    Added functionality such that emojis can now be sent, received, and seen in full colour.

--- SignalRChat/ChatHub.cs
+++ SignalRChat/ChatHub.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SignalRChat
 {
@@ -254,5 +255,6 @@ namespace SignalRChat
 
             }
         }
+
     }
 }
